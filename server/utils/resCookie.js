33cabe73e9dfc60132e89ae748def8cf0d6ff765
Bo Bo Aung <45998100+boboaungdev@users.noreply.github.com>
2025-12-04 22:22:10 +0700

    Remove maxAge from cookie options
    Commented out the maxAge property in cookie options.

--- server/utils/resCookie.js
+++ server/utils/resCookie.js
@@ -5,7 +5,7 @@ const resCookie = (req, res, key, value, options = {}) => {
     httpOnly: true,
     sameSite: isLocalhost ? "Lax" : "None",
     secure: !isLocalhost,
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  //  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     ...options,
   });
 };
